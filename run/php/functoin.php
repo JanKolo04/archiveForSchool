27b59eb492b7127f17a1b791833c1338dc46faa4
JanKolo04 <jankolodziej04@gmail.com>
2022-02-17 11:25:06 +0100

    echo only one photo after serach

--- run/php/functoin.php
+++ run/php/functoin.php
@@ -53,12 +53,11 @@
 			//podzielenie stringa na wyrazy które stworzą tablice
 			$arrayText = explode(" ", $data);
 
-
 			//tablica z poprawnymi wynikami
 			$arrayCheck = [];
 			//petla do sprawdzania czy dany wyraz pasuje do klucza
 			//pierwsza pętla zajmuje sie tym zeby przejsc po kazdej tablicy w tablicy wielowymiarowej
-			for($i=0; $i<3; $i++) {
+			for($i=0; $i<sizeof($arrayAll); $i++) {
 				//druga jest od tego żeby sprawdzic czy Imie, Nazwisko i Zdjecie mają dopasowanie
 				for($y=0; $y<3; $y++) {
 					//porówanie danych czy Imie pasuje do wartości która wpisaliśmy
@@ -72,8 +71,53 @@
 					}
 					//tutaj do nazwy pracy
 					else if($arrayAll[$i]['work_name'] == $arrayText[$y]) {
-						$arrayCheck["work_name"] = $arrayText[$y];
-						//tutaj jeszcze dodajemy Profil i Klase
+						/*
+							---------FUNCKJA PODMIANY ROZSZERZEN-------
+							ta funkcja jest potrzebna poniewaz php nie obsługuje 
+							rozszerzen JPEG wiec jest potrzebna ta funkcja
+						*/
+						//zniemnne do zmiany rozszerzenia gdy zdjecie jest jpeg
+						//zmienna rozszerzenie
+						$roz = "";
+						//sama nazwa bez rozszerzenia
+						$only_work_name = "";
+						//zmienna z work_name
+						$text = $arrayText[$y];
+						//pelta odpowiedzialna za znalezienie kropki
+						for($x=0; $x<strlen($text); $x++) {
+							//jeśli znak w zmiennej text jest kropka to tworzy zmienna 
+							//z lokalizacja kropki
+							if($text[$x] == '.') {
+								//zmienna kropka
+								$dot=$x;
+							}
+						}
+						//pelta odpowiedzialna za dodanie do zmiennej only_work_name
+						//samej nazwy bez rozszerzenia
+						for($a=0; $a<$dot; $a++) {
+							//nadpisywanie zmiennej
+							$only_work_name.=$text[$a];
+						}
+						//petla która dodaje do zmiennej roz
+						//nazwe rozszerzenia
+						for($z=$dot+1; $z<strlen($text); $z++) {
+							//nadpisywanie zmiennje
+							$roz.=$text[$z];
+						}
+						//jesli rozszerzenie jest jpeg to zmienia je na jpg
+						if($roz == 'jpeg') {
+							//podmiana
+							$roz ='jpg';
+							//przypisywanie wartości do klucza
+							$arrayCheck["work_name"] = $only_work_name.".".$roz;
+						}
+						//jesli rozszerzenie nie jest jpeg to przypisuje wartośc 
+						//work_name bez zmian
+						else {
+							//przypisywanie wartości do klucza
+							$arrayCheck['work_name'] = $arrayText[$y];
+						}
+						//tutaj dodajemy Profil i Klase
 						$arrayCheck["Profil"] = $arrayAll[$i]['Profil'];
 						$arrayCheck["Klasa"] = $arrayAll[$i]['Klasa'];
 					}
@@ -84,18 +128,22 @@
 			if(empty($arrayCheck)) {
 				echo "User dosent exist";
 			}
-			
+
 
 			//wyświetlanie wszytskich zdjec ucznia
 			//tworzenie sciezki
-			$path = "../{$arrayCheck['Profil']}/{$arrayCheck['Klasa']}/{$arrayCheck['Imie']} {$arrayCheck['Nazwisko']}";
+			$path = "../{$arrayCheck['Profil']}/{$arrayCheck['Klasa']}/{$arrayCheck['Imie']} {$arrayCheck['Nazwisko']}/{$arrayCheck['work_name']}";
+			echo "<img src='$path' class='img'>";
 
+
+			/*
+			//wyświetlanie każdego zdjęcia z folderu ucznia
 			//funkcja sprawdzająca czy otworzy sie folder
 			if($d = opendir($path)) {
 				//wczytywanie zdjec dopóki nie wczyta wszystkich
 				while($file = readdir($d)) {
 					//jesli zdjecie nie ma nazwy . lub .. to twórz
-					if($file != '.' && $file != '..') {
+					if($file != '.' && $file != '..' && $file==$arrayCheck['work_name']) {
 						//dodawanie nazwy zdjecia do scieżki
 						$exist = $path."/".$file;
 						//tworzenie zdjęcia
@@ -105,6 +153,8 @@
 				//zamykanie folderu
 				closedir($d);
 			}
+
+			*/
 
 
 
