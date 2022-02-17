2b58145700980753eac8cdcdc871e5f68e8b7980
JanKolo04 <jankolodziej04@gmail.com>
2022-02-17 11:40:06 +0100

    security after submit

--- run/php/functoin.php
+++ run/php/functoin.php
@@ -51,89 +51,99 @@
 				}
 			}
 			//podzielenie stringa na wyrazy które stworzą tablice
-			$arrayText = explode(" ", $data);
 
-			//tablica z poprawnymi wynikami
-			$arrayCheck = [];
-			//petla do sprawdzania czy dany wyraz pasuje do klucza
-			//pierwsza pętla zajmuje sie tym zeby przejsc po kazdej tablicy w tablicy wielowymiarowej
-			for($i=0; $i<sizeof($arrayAll); $i++) {
-				//druga jest od tego żeby sprawdzic czy Imie, Nazwisko i Zdjecie mają dopasowanie
-				for($y=0; $y<3; $y++) {
-					//porówanie danych czy Imie pasuje do wartości która wpisaliśmy
-					if($arrayAll[$i]['Imie'] == $arrayText[$y]) {
-						//tutaj tworze klucz Imie w tablicy arrayCheck i dodaje kluczowi wartość
-						$arrayCheck["Imie"] = $arrayText[$y];
-					}
-					//tutaj jest to samo porównanie co powyżej tylko że tutaj do nazwiska
-					else if($arrayAll[$i]['Nazwisko'] == $arrayText[$y]) {
-						$arrayCheck["Nazwisko"] = $arrayText[$y];
-					}
-					//tutaj do nazwy pracy
-					else if($arrayAll[$i]['work_name'] == $arrayText[$y]) {
-						/*
-							---------FUNCKJA PODMIANY ROZSZERZEN-------
-							ta funkcja jest potrzebna poniewaz php nie obsługuje 
-							rozszerzen JPEG wiec jest potrzebna ta funkcja
-						*/
-						//zniemnne do zmiany rozszerzenia gdy zdjecie jest jpeg
-						//zmienna rozszerzenie
-						$roz = "";
-						//sama nazwa bez rozszerzenia
-						$only_work_name = "";
-						//zmienna z work_name
-						$text = $arrayText[$y];
-						//pelta odpowiedzialna za znalezienie kropki
-						for($x=0; $x<strlen($text); $x++) {
-							//jeśli znak w zmiennej text jest kropka to tworzy zmienna 
-							//z lokalizacja kropki
-							if($text[$x] == '.') {
-								//zmienna kropka
-								$dot=$x;
+			$arrayText = explode(" ", $data);
+			
+			//jesli uzytkownik nie wpisze 3 wyrazów to pokaże sie alert
+			//z Error
+			if(sizeof($arrayText) != 3) {
+				echo "<script>alert('Error')</script>";
+			}
+			//jesli wpisał 3 to wykonuje sie kod dalej
+			else {
+				//tablica z poprawnymi wynikami
+				$arrayCheck = [];
+				//petla do sprawdzania czy dany wyraz pasuje do klucza
+				//pierwsza pętla zajmuje sie tym zeby przejsc po kazdej tablicy w tablicy wielowymiarowej
+				for($i=0; $i<sizeof($arrayAll); $i++) {
+					//druga jest od tego żeby sprawdzic czy Imie, Nazwisko i Zdjecie mają dopasowanie
+					for($y=0; $y<3; $y++) {
+						//porówanie danych czy Imie pasuje do wartości która wpisaliśmy
+						if($arrayAll[$i]['Imie'] == $arrayText[$y]) {
+							//tutaj tworze klucz Imie w tablicy arrayCheck i dodaje kluczowi wartość
+							$arrayCheck["Imie"] = $arrayText[$y];
+						}
+						//tutaj jest to samo porównanie co powyżej tylko że tutaj do nazwiska
+						else if($arrayAll[$i]['Nazwisko'] == $arrayText[$y]) {
+							$arrayCheck["Nazwisko"] = $arrayText[$y];
+						}
+						//tutaj do nazwy pracy
+						else if($arrayAll[$i]['work_name'] == $arrayText[$y]) {
+							/*
+								---------FUNCKJA PODMIANY ROZSZERZEN-------
+								ta funkcja jest potrzebna poniewaz php nie obsługuje 
+								rozszerzen JPEG wiec jest potrzebna ta funkcja
+							*/
+							//zniemnne do zmiany rozszerzenia gdy zdjecie jest jpeg
+							//zmienna rozszerzenie
+							$roz = "";
+							//sama nazwa bez rozszerzenia
+							$only_work_name = "";
+							//zmienna z work_name
+							$text = $arrayText[$y];
+							//pelta odpowiedzialna za znalezienie kropki
+							for($x=0; $x<strlen($text); $x++) {
+								//jeśli znak w zmiennej text jest kropka to tworzy zmienna 
+								//z lokalizacja kropki
+								if($text[$x] == '.') {
+									//zmienna kropka
+									$dot=$x;
+								}
 							}
+							//pelta odpowiedzialna za dodanie do zmiennej only_work_name
+							//samej nazwy bez rozszerzenia
+							for($a=0; $a<$dot; $a++) {
+								//nadpisywanie zmiennej
+								$only_work_name.=$text[$a];
+							}
+							//petla która dodaje do zmiennej roz
+							//nazwe rozszerzenia
+							for($z=$dot+1; $z<strlen($text); $z++) {
+								//nadpisywanie zmiennje
+								$roz.=$text[$z];
+							}
+							//jesli rozszerzenie jest jpeg to zmienia je na jpg
+							if($roz == 'jpeg') {
+								//podmiana
+								$roz ='jpg';
+								//przypisywanie wartości do klucza
+								$arrayCheck["work_name"] = $only_work_name.".".$roz;
+							}
+							//jesli rozszerzenie nie jest jpeg to przypisuje wartośc 
+							//work_name bez zmian
+							else {
+								//przypisywanie wartości do klucza
+								$arrayCheck['work_name'] = $arrayText[$y];
+							}
+							//tutaj dodajemy Profil i Klase
+							$arrayCheck["Profil"] = $arrayAll[$i]['Profil'];
+							$arrayCheck["Klasa"] = $arrayAll[$i]['Klasa'];
 						}
-						//pelta odpowiedzialna za dodanie do zmiennej only_work_name
-						//samej nazwy bez rozszerzenia
-						for($a=0; $a<$dot; $a++) {
-							//nadpisywanie zmiennej
-							$only_work_name.=$text[$a];
-						}
-						//petla która dodaje do zmiennej roz
-						//nazwe rozszerzenia
-						for($z=$dot+1; $z<strlen($text); $z++) {
-							//nadpisywanie zmiennje
-							$roz.=$text[$z];
-						}
-						//jesli rozszerzenie jest jpeg to zmienia je na jpg
-						if($roz == 'jpeg') {
-							//podmiana
-							$roz ='jpg';
-							//przypisywanie wartości do klucza
-							$arrayCheck["work_name"] = $only_work_name.".".$roz;
-						}
-						//jesli rozszerzenie nie jest jpeg to przypisuje wartośc 
-						//work_name bez zmian
-						else {
-							//przypisywanie wartości do klucza
-							$arrayCheck['work_name'] = $arrayText[$y];
-						}
-						//tutaj dodajemy Profil i Klase
-						$arrayCheck["Profil"] = $arrayAll[$i]['Profil'];
-						$arrayCheck["Klasa"] = $arrayAll[$i]['Klasa'];
 					}
 				}
+				//sprawdzamy czy dane zosatły dopasowane czy nie
+				//jesli dane nie zostały dopasowane to wtedy pokazuje
+				//sie alert ze nie ma takiego uzytkownika
+				if(empty($arrayCheck)) {
+					echo "<script>alert('User dosen't exist')</script>";
+				}
+				else {
+					//wyświetlanie wszytskich zdjec ucznia
+					//tworzenie sciezki
+					$path = "../{$arrayCheck['Profil']}/{$arrayCheck['Klasa']}/{$arrayCheck['Imie']} {$arrayCheck['Nazwisko']}/{$arrayCheck['work_name']}";
+					echo "<img src='$path' class='img'>";
+				}
 			}
-
-			//sprawdzamy czy dane zosatły dopasowane czy nie
-			if(empty($arrayCheck)) {
-				echo "User dosent exist";
-			}
-
-
-			//wyświetlanie wszytskich zdjec ucznia
-			//tworzenie sciezki
-			$path = "../{$arrayCheck['Profil']}/{$arrayCheck['Klasa']}/{$arrayCheck['Imie']} {$arrayCheck['Nazwisko']}/{$arrayCheck['work_name']}";
-			echo "<img src='$path' class='img'>";
 
 
 			/*
