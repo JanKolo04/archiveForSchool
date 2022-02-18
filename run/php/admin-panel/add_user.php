fc2a8a9bafeae4d6f1320577edd8ec65c1776f79
JanKolo04 <jankolodziej04@gmail.com>
2022-02-18 15:08:31 +0100

    upload files to direcotry with max size and checikng is exist

--- run/php/admin-panel/add_user.php
+++ run/php/admin-panel/add_user.php
@@ -7,16 +7,21 @@
 </head>
 <body>
 
-	<form method="post">
+	<form method="post" enctype="multipart/form-data">
 		<select name="users" id="users">
 			<option>--Select--</option>
 		</select>
+		<input type="file" name="file">
 		<button name="submit" type="submit">Get</button>
 	</form>
 
 	<?php
 
 		include("../connection.php");
+
+		if(isset($_POST['submit'])) {
+			send();
+		}
 
 		function get_user() {
 			global $con, $arrayAll;
@@ -33,13 +38,66 @@
 				}
 			}
 		}
-		
-		/*
 
-			-----TOD DO-----
-			1.Pobranie pierwszej wartości z optin czyli !!!ID!!!
+		function send() {
+			global $con;
 
-		*/
+			$selected = $_POST['users'];
+			$arrayData = explode(" ",$selected);
+			$id = $arrayData[0];
+			$name = $arrayData[1];
+			$lastname = $arrayData[2];
+			$class = $arrayData[3];
+			$profil = $arrayData[4];
+
+		   	if(isset($_FILES['file'])) {
+				$errors= array();
+				$fileName = $_FILES['file']['name'];
+				$fileSize =$_FILES['file']['size'];
+				$fileTmp =$_FILES['file']['tmp_name'];
+				$dir = '../all/';
+
+				$explode = explode('.',$_FILES['file']['name']);
+				$fileExt = end($explode);
+
+				//max file size
+				$maxSize = 5*(1048576);
+			    $extensions= array("jpg","png");
+
+			    while(empty($errors)) {
+				    if(file_exists($dir.$fileName)) {
+				    	$errors[] = 'File exist';
+				     	echo("<script>alert('File exist');</script>");
+				     	break;
+
+				    }
+				      
+				    else if(!in_array($fileExt,$extensions)) {
+				        $errors[] = 'Different extensions, use JPG or PNG';
+				        echo("<script>alert('Different extensions, use JPG or PNG');</script>");
+				        break;
+				    }
+
+	      			else if($fileSize > $maxSize) {
+	        			$errors[] = 'File is biger than 3MB';
+	        			echo("<script>alert('File is biger than 3MB');</script>");
+	        			echo $fileSize;
+	        			break;
+	      			}
+
+					else if(empty($errors)) {
+						move_uploaded_file($fileTmp,$dir.$fileName);
+
+						$sendSQL = "INSERT INTO works(Imie, Nazwisko, Klasa, id_user, work_name, Profil) VALUES('$name', '$lastname', '$class', '$id', '$fileName', '$profil')";
+						$queryInsertWork = mysqli_query($con, $sendSQL);
+						break;
+
+					}
+				}
+
+		   	}
+
+		}
 
 		get_user();
 
