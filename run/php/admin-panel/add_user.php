07ca146d08304e7be778bdca538df00ec04693ae
JanKolo04 <jankolodziej04@gmail.com>
2022-02-21 10:35:11 +0100

    new name for files and change function names

--- run/php/admin-panel/add_user.php
+++ run/php/admin-panel/add_user.php
@@ -3,127 +3,54 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Add file</title>
+	<title>Add user</title>
 </head>
 <body>
 
-	<form method="post" enctype="multipart/form-data">
-		<select name="users" id="users">
+	<form method="post">
+		<input type="text" name="imie">
+		<input type="text" name="nazwisko">
+		<select name="klasa">
 			<option>--Select--</option>
+			<option>3a</option>
+			<option>3b</option>
 		</select>
-		<input type="file" name="file">
-		<button name="submit" type="submit">Get</button>
+		<select name="sepcjalizacja">
+			<option>--Select--</option>
+			<option>Informatyka</option>
+			<option>Grafika</option>
+		</select>
+
+		<button type="submit" name="add">Add</button>
 	</form>
 
 	<?php
 
 		include("../connection.php");
 
-		if(isset($_POST['submit'])) {
-			send();
+		if(isset($_POST['add'])) {
+			add();
 		}
 
-		function get_user() {
-			global $con, $arrayAll;
-
-			$get = "SELECT * FROM users";
-			$queryGet = mysqli_query($con, $get);
-
-			$arrayAll = [""];
-			$i = 0;
-			while($row = mysqli_fetch_array($queryGet)) {
-				foreach($queryGet as $key=>$value) {
-					$arrayAll[$i] = $value;
-					$i++;
-				}
-			}
-		}
-
-		function send() {
+		function add() {
 			global $con;
 
-			$selected = $_POST['users'];
-			$arrayData = explode(" ",$selected);
-			$id = $arrayData[0];
-			$name = $arrayData[1];
-			$lastname = $arrayData[2];
-			$class = $arrayData[3];
-			$profil = $arrayData[4];
+			//name
+			$name = $_POST['imie'];
+			//lastname
+			$lastname = $_POST['nazwisko'];
+			//class
+			$class = $_POST['klasa'];
+			//profile
+			$profil = $_POST['sepcjalizacja'];
 
-		   	if(isset($_FILES['file'])) {
-				$errors= array();
-				$fileName = $_FILES['file']['name'];
-				$fileSize =$_FILES['file']['size'];
-				$fileTmp =$_FILES['file']['tmp_name'];
-				$dir = '../all/';
-
-				$explode = explode('.',$_FILES['file']['name']);
-				$fileExt = end($explode);
-
-				//max file size
-				$maxSize = 5*(1048576);
-			    $extensions= array("jpg","png");
-
-			    while(empty($errors)) {
-				    if(file_exists($dir.$fileName)) {
-				    	$errors[] = 'File exist';
-				     	echo("<script>alert('File exist');</script>");
-				     	break;
-
-				    }
-				      
-				    else if(!in_array($fileExt,$extensions)) {
-				        $errors[] = 'Different extensions, use JPG or PNG';
-				        echo("<script>alert('Different extensions, use JPG or PNG');</script>");
-				        break;
-				    }
-
-	      			else if($fileSize > $maxSize) {
-	        			$errors[] = 'File is biger than 3MB';
-	        			echo("<script>alert('File is biger than 3MB');</script>");
-	        			echo $fileSize;
-	        			break;
-	      			}
-
-					else if(empty($errors)) {
-						move_uploaded_file($fileTmp,$dir.$fileName);
-
-						$sendSQL = "INSERT INTO works(Imie, Nazwisko, Klasa, id_user, work_name, Profil) VALUES('$name', '$lastname', '$class', '$id', '$fileName', '$profil')";
-						$queryInsertWork = mysqli_query($con, $sendSQL);
-						break;
-
-					}
-				}
-
-		   	}
-
+			//insert data into user
+			$insertSQL = "INSERT INTO users(Imie, Nazwisko, Klasa, Profil) VALUES('$name', '$lastname', '$class', '$profil')";
+			//quert add user
+			$insertQuery = mysqli_query($con, $insertSQL);
 		}
-
-		get_user();
 
 	?>
-
-	<script type="text/javascript">
-		
-		function print_all_data() {
-			const arrayAll = <?php echo json_encode($arrayAll); ?>;
-
-			const select = document.querySelector('#users');
-
-			let len = arrayAll.length;
-			for(let i=0; i<len; ++i) {
-				let text = arrayAll[i]["id"]+" "+arrayAll[i]['Imie']+" "+arrayAll[i]['Nazwisko']+" "+arrayAll[i]['Klasa']+" "+arrayAll[i]['Profil'];
-
-				let option = document.createElement("option");
-				option.class = "option";
-				option.innerHTML = text;
-				select.appendChild(option);
-			}
-		}
-
-		print_all_data();
-
-	</script>
 
 </body>
 </html>
---
+++ run/php/admin-panel/add_work_for_user.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Add file</title>
+</head>
+<body>
+
+	<form method="post" enctype="multipart/form-data">
+		<select name="users" id="users">
+			<option>--Select--</option>
+		</select>
+		<input type="file" name="file">
+		<button name="submit" type="submit">Get</button>
+	</form>
+
+	<?php
+
+		include("../connection.php");
+
+		if(isset($_POST['submit'])) {
+			send_file();
+		}
+
+		function add_all_user_to_array() {
+			global $con, $arrayAll;
+
+			$get = "SELECT * FROM users";
+			$queryGet = mysqli_query($con, $get);
+
+			$arrayAll = [""];
+			$i = 0;
+			while($row = mysqli_fetch_array($queryGet)) {
+				foreach($queryGet as $key=>$value) {
+					$arrayAll[$i] = $value;
+					$i++;
+				}
+			}
+		}
+
+		function send_file() {
+			global $con;
+
+			$selected = $_POST['users'];
+			$arrayData = explode(" ",$selected);
+			$id = $arrayData[0];
+			$name = $arrayData[1];
+			$lastname = $arrayData[2];
+			$class = $arrayData[3];
+			$profil = $arrayData[4];
+
+		   	if(isset($_FILES['file'])) {
+				$errors= array();
+				$fileName = $_FILES['file']['name'];
+				$fileSize =$_FILES['file']['size'];
+				$fileTmp =$_FILES['file']['tmp_name'];
+				$dir = '../all/';
+
+				$explode = explode('.',$_FILES['file']['name']);
+				$fileExt = end($explode);
+
+				//max file size
+				$maxSize = 5*(1048576);
+			    $extensions= array("jpg","png");
+
+			    while(empty($errors)) {
+				    if(file_exists($dir.$fileName)) {
+				    	$errors[] = 'File exist';
+				     	echo("<script>alert('File exist');</script>");
+				     	break;
+
+				    }
+				      
+				    else if(!in_array($fileExt,$extensions)) {
+				        $errors[] = 'Different extensions, use JPG or PNG';
+				        echo("<script>alert('Different extensions, use JPG or PNG');</script>");
+				        break;
+				    }
+
+	      			else if($fileSize > $maxSize) {
+	        			$errors[] = 'File is biger than 3MB';
+	        			echo("<script>alert('File is biger than 3MB');</script>");
+	        			echo $fileSize;
+	        			break;
+	      			}
+
+					else if(empty($errors)) {
+						move_uploaded_file($fileTmp,$dir.$fileName);
+
+						$sendSQL = "INSERT INTO works(Imie, Nazwisko, Klasa, id_user, work_name, Profil) VALUES('$name', '$lastname', '$class', '$id', '$fileName', '$profil')";
+						$queryInsertWork = mysqli_query($con, $sendSQL);
+						break;
+
+					}
+				}
+
+		   	}
+
+		}
+
+		add_all_user_to_array();
+
+	?>
+
+	<script type="text/javascript">
+		
+		function print_all_users() {
+			const arrayAll = <?php echo json_encode($arrayAll); ?>;
+
+			const select = document.querySelector('#users');
+
+			let len = arrayAll.length;
+			for(let i=0; i<len; ++i) {
+				let text = arrayAll[i]["id"]+" "+arrayAll[i]['Imie']+" "+arrayAll[i]['Nazwisko']+" "+arrayAll[i]['Klasa']+" "+arrayAll[i]['Profil'];
+
+				let option = document.createElement("option");
+				option.class = "option";
+				option.innerHTML = text;
+				select.appendChild(option);
+			}
+		}
+
+		print_all_users();
+
+	</script>
+
+</body>
+</html>
