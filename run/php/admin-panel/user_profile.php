0f82b6c3f17b9afef53e2c7b5e0c4fcfe378b42a
JanKolo04 <jankolodziej04@gmail.com>
2022-02-23 16:12:56 +0100

    function in php to update data

--- run/php/admin-panel/user_profile.php
+++ run/php/admin-panel/user_profile.php
@@ -118,6 +118,10 @@
 			add_file_into_database_and_directory();
 		}
 
+		else if(isset($_POST['editSubmit'])) {
+			change_work_name_or_descriptoin();
+		}
+
 		function get_id() {
 			global $con, $arrayWithDataFromQuery, $arrayWithWorks;
 			//get user id from url
@@ -174,6 +178,55 @@
 		}	
 
 		get_id();
+
+		function change_work_name_or_descriptoin() {
+			global $con;
+			//get data from edit inputs
+			//work name input
+			$workNameEdit = $_POST['editWork_name'];
+			//description input
+			$descriptionEdit = $_POST['editDescription'];
+
+			//optoin value
+			$optionValue = $_POST['selectWorkToEdit'];
+
+			//sql query
+			$sqlCheck = "SELECT work_name, description FROM user_works WHERE id='$optionValue'";
+			if($queryCheck = mysqli_query($con, $sqlCheck)) {
+				if($queryCheck->num_rows > 0) {
+					//array for data from sqlCheck
+					$arrayWithCheckData = [];
+					while($row = mysqli_fetch_array($queryCheck)) {
+						//append data
+						$arrayWithCheckData = [
+							"work_name"=>$row['work_name'],
+							"description"=>$row['description']
+						];
+					}
+
+					//if work is different or description then data in db do update db
+					if($arrayWithCheckData['work_name'] != $workNameEdit || $arrayWithCheckData['description'] != $descriptionEdit) {
+						echo $arrayWithCheckData['work_name']." ".$workNameEdit;
+						//update data
+						$sqlChange = "UPDATE user_works SET work_name='$workNameEdit', description='$descriptionEdit' WHERE id='$optionValue'";
+						echo $sqlChange;
+						$queryChange = mysqli_query($con, $sqlChange);
+					}
+
+
+					//if data is same code will return alert
+					else if(($arrayWithCheckData['work_name'] == $workNameEdit) && ($arrayWithCheckData['description'] == $descriptionEdit)) {
+						//alert
+						echo "<script>alert('Data is same');</script>";
+					}
+					//if error return alert with error
+					else {
+						echo "<script>alert('Error');</script>";
+					}
+				}
+			}
+
+		}
 
 
 		function add_file_into_database_and_directory() {
@@ -279,7 +332,7 @@
 				for(let i=0; i<arrayWorksLen; ++i) {
 					//create option
 					let option = document.createElement("option");
-					option.id = "option";
+					option.className = "option";
 					option.value = arrayWorks[i]['id_work'];
 					option.innerHTML = arrayWorks[i]['work_name'];
 					select.appendChild(option);
