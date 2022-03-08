3ef9f81236aa791c82b0378c528ea307d0060200
JanKolo04 <jankolodziej04@gmail.com>
2022-03-08 10:18:39 +0100

    new version of gallery

--- run/html/underpages/js/script-contestPage.js
+++ run/html/underpages/js/script-contestPage.js
@@ -1,14 +1,4 @@
-//need this to deactivate lightbox on small screens
-$(document).ready(function () {
-  
-  lightboxOnResize();
- 
-});
 
-$(window).resize(function() {
-  lightboxOnResize();
-
-});
 
 //***ISOTOPE***
 // init Isotope
@@ -23,61 +13,3 @@ $('.filter-button-group').on( 'click', 'button', function() {
   $grid.isotope({ filter: filterValue });
 });
 
-
-
-/*
-$('#filters a').on("click", function(){
-  var selector = $(this).attr('data-filter'); 
-  $('#container').isotope({ filter: selector }, function(){
-    if(selector == "*"){
-     $(".fancybox").attr("data-fancybox-group", "gallery");
-    } else{ 
-     $(selector).find(".fancybox").attr("data-fancybox-group", selector);
-    }
-  });
-  return false;
-});
-*/
-
-
-
-// change is-checked class on buttons
-$('.btn-group').each( function( i, buttonGroup ) {
-  var $buttonGroup = $( buttonGroup );
-  $buttonGroup.on( 'click', 'button', function() {
-    $buttonGroup.find('.is-checked').removeClass('is-checked');
-    $( this ).addClass('is-checked');
-  });
-});
-
-//***LIGHTBOX***
-//https://www.no-margin-for-errors.com/projects/prettyphoto-jquery-lightbox-clone/documentation
-// $(document).delegate('a[rel="prettyPhoto[portfolio]"]', 'click', function(event) {
-//     event.preventDefault();
-//     $(this).prettyPhoto({
-//       theme: "light_square",
-//     });
-// }); 
-
-function lightboxOnResize() {
-  
-  if ($(window).width() < 768) {       
-    $('a[rel="prettyPhoto[portfolio]"]')
-        .removeAttr('rel')
-        .addClass('lightboxRemoved');
-      
-    $('a.lightboxRemoved').click(function( event ) {
-            event.preventDefault();
-      console.log("test");
-          });
-     // $("a[rel='prettyPhoto[portfolio]']").unbind('click');
-    
-  } 
-  else {        
-    $('a.lightboxRemoved').attr('rel', 'prettyPhoto[portfolio]').removeClass("lightboxRemoved");
-    $("a[rel='prettyPhoto[portfolio]']").prettyPhoto({
-      theme: "light_square",
-    });
-        
-  }
-}
